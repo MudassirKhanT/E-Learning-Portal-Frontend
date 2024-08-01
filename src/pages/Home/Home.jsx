59a7b3cb110bf1abcd1bbd967f6b7ceb908b3db9
MudassirKhanT <mudassirkhantailor@gmail.com>
2024-08-01 20:29:13 +0530

    add home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,19 @@
 import React from "react";
-
+import { useNavigate } from "react-router-dom";
+import "./home.css";
 const Home = () => {
-  return <div>Home</div>;
+  const navigate = useNavigate();
+  return (
+    <div className="home">
+      <div className="home-content">
+        <h1>Welcome to out E-learing Platform</h1>
+        <p>Learn, Grow, Excel</p>
+        <button className="common-btn" onClick={() => navigate("/courses")}>
+          Get Started
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
